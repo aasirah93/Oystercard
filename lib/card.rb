0ee91d3bfb10478cea48d29fa49118236f2075cb
tracht <marija.trachtenberg@gmail.com>
2020-04-20 15:51:34 +0100

    tests that new instance of Card has default balance of 0

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,8 @@
 class Card
+  attr_reader :balance
+
+  def initialize(balance=0)
+    @balance = balance
+  end
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,8 @@
 require "card"
 
 describe Card do
+  it "new instance of Card starts with 0 balance" do
+    new_card = Card.new
+    expect(new_card.balance).to equal(0)
+  end
 end
