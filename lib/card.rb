dd4f5da394bdf56a95ecebf479f97db43cbded8b
tracht <marija.trachtenberg@gmail.com>
2020-04-20 16:38:55 +0100

    test top-up edge cases for negative number; haven't worked out string test case

--- lib/card.rb
+++ lib/card.rb
@@ -5,4 +5,16 @@ class Card
     @balance = balance
   end
 
+  def top_up(amount)
+    amount.to_i
+
+    if amount < 0
+      raise ArgumentError.new('Enter positive number')
+    elsif amount == amount.to_i
+      @balance += amount
+    else
+      raise ArgumentError.new('Enter positive number')
+    end
+  end
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,20 @@
 require "card"
 
 describe Card do
+  new_card = Card.new
   it "new instance of Card starts with 0 balance" do
-    new_card = Card.new
     expect(new_card.balance).to equal(0)
+  end
+
+  it 'user cant top up with negative number' do
+    expect{new_card.top_up(-5)}.to raise_error("Enter positive number")
+  end
+
+  it 'user cant top up with non-number' do
+    expect{new_card.top_up("hi").to raise_error("Enter a number")}
+  end
+
+  it 'topping up by £10 incr balance by £10' do
+    expect{new_card.top_up(10)}.to change {new_card.balance}.by(10)
   end
 end
